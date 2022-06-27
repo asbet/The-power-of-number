d953672fd61a9fd206a35fcb094c78d089ef2303
asbet <81320359+asbet@users.noreply.github.com>
2022-06-27 20:23:56 +0300

    ThePowerOf2345
    Number's power of:2,3,4,5

---
+++ ThePowerof2345/app/src/main/java/com/betulas/thepowerof2345/MainActivity.java
@@ -0,0 +1,55 @@
+package com.betulas.thepowerof2345;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+   EditText enter_number;
+   EditText result_number;
+   TextView textView;
+    //primitive=ilkel
+    //heap=yığın =birikinti
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        enter_number=findViewById(R.id.enter_number);
+        textView=findViewById(R.id.textView2);
+
+    }
+    public void thepowerof2(View view){
+    int enter_number1=Integer.parseInt(enter_number.getText().toString());
+    int result=enter_number1*enter_number1;
+    textView.setText("result: "+result);
+    }
+
+    public void thepowerof3(View view){
+        int enter_number1=Integer.parseInt((enter_number.getText().toString()));
+        int result=enter_number1*enter_number1*enter_number1;
+        textView.setText("result: "+result);
+    }
+    public void thepowerof4(View view){
+        int enter_number1=Integer.parseInt(enter_number.getText().toString());
+        int result=enter_number1*enter_number1*enter_number1*enter_number1;
+        textView.setText("result: " +result);
+    }
+    public void thepowerof5(View view){
+        int enter_number1=Integer.parseInt(enter_number.getText().toString());
+        int result=enter_number1*enter_number1*enter_number1*enter_number1*enter_number1;
+        textView.setText("result: " +result);
+    }
+
+}
+
+
+//sayıyla kendi sayıyı çarpıp tuşa basılan rakam kadar o sayıyı çarpmamımz gerekiyor
